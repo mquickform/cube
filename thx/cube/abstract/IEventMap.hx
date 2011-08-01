521400cd97f31dd1cb76f6dc9a22138e8a9da584
James Jackson <jamesbjackson@gmail.com>
2011-08-01 16:31:06 +0100

    removing thx folder

--- thx/cube/abstract/IEventMap.hx
+++
@@ -1,6 +0,0 @@
-package thx.cube.abstract;
-
-interface IEventMap<E> {
-	function unmapListeners() : Void;
-	function routeEventToListener( event : E, listener : E->Void, originalEventClass : Class<E> ) : Void;
-}
--- thx/cube/abstract/IView.hx
+++
@@ -1,5 +0,0 @@
-package thx.cube.abstract;
-
-interface IView {
-
-}
